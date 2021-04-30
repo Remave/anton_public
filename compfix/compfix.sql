97b80329c4cbd0474da0b0d4f13d640e9ddaf5fe
Anton Rum <53833677+Remave@users.noreply.github.com>
2021-04-30 19:28:21 +0300

    CompFix

---
+++ compfix/compfix.sql
@@ -0,0 +1,160 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Апр 30 2021 г., 18:09
+-- Версия сервера: 10.3.13-MariaDB-log
+-- Версия PHP: 7.1.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `compfix`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `rating`
+--
+
+CREATE TABLE `rating` (
+  `id` int(11) NOT NULL,
+  `rev_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `rate` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `rating`
+--
+
+INSERT INTO `rating` (`id`, `rev_id`, `user_id`, `rate`) VALUES
+(31, 1, 3, 1),
+(32, 3, 3, 1),
+(33, 4, 3, 1),
+(36, 4, 2, 1),
+(37, 3, 4, -1),
+(38, 4, 4, 1),
+(40, 7, 3, -1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` int(10) NOT NULL,
+  `owner` int(11) NOT NULL,
+  `header` text NOT NULL,
+  `text` text NOT NULL,
+  `rating` int(9) NOT NULL DEFAULT 0,
+  `approved` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `owner`, `header`, `text`, `rating`, `approved`) VALUES
+(1, 2, 'sdfgdsgf', 'qwrgwrgwsfgdsfg', 1, 1),
+(3, 2, 'as dsfgsfgsdfg', 'ashlksdg af fasdghfasdjfgasjd fgasjdfg jasdgf hasgj gfasdfgaskjdf jksfg s', 0, 1),
+(4, 2, 'LOREM', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 3, 1),
+(5, 4, '32656436t', 'ergdfbvbsd fgdsf gsdfgaskdgfasjdgfjas ghbxcvs afvjas djvasjhf gvjasdg vjkafjvgasjdv gasdgvjkasjdv s sf sd fhsdgkfjs', 0, 0),
+(7, 2, 'fshsghdgsdgh', 'ssdghsdfghfsghdfghdfgh', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` varchar(32) NOT NULL,
+  `pass` varchar(64) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `type` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `pass`, `email`, `type`) VALUES
+(1, 'sdgasdf', 'sfd', 'sfgfgdf', 0),
+(2, '123', '123', '123', 0),
+(3, 'admin', 'admin', '123@123', 1),
+(4, '5324532', '123', '2345325', 0);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `rating`
+--
+ALTER TABLE `rating`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_owner` (`owner`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `rating`
+--
+ALTER TABLE `rating`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT для таблицы `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `FK_owner` FOREIGN KEY (`owner`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
